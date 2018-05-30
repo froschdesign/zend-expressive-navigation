78028cd9dd0f21872e7f99ebc7944069274dfd4b
Frank Brückner <dev@froschdesignstudio.de>
2018-05-30 12:59:11 +0200

    Updates licence header

--- src/Service/ExpressiveNavigationAbstractServiceFactory.php
+++ src/Service/ExpressiveNavigationAbstractServiceFactory.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @see       https://github.com/zendframework/zend-navigation for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-navigation/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/zendframework/zend-expressive-navigation for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-navigation/blob/master/LICENSE.md New BSD License
  */
 
 namespace Zend\Expressive\Navigation\Service;
